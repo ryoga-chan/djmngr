059def5f6882d60fdb662afd23dcb597d72c6c57
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-04-24 15:55:13 +0200

    * doujin - use JapaneseLabels concern * doujinshi view - show more info + sample images

--- app/models/concerns/japanese_labels.rb
+++ app/models/concerns/japanese_labels.rb
@@ -2,7 +2,7 @@ module JapaneseLabels
   extend ActiveSupport::Concern
   
   included do
-    def label_name_orig   = self.name.present?        ? self.name        : self.name_kana
-    def label_name_romaji = self.name_romaji.present? ? self.name_romaji : self.name_kakasi
+    def label_name_kanji = self.name.present?        ? self.name        : self.try(:name_kana)
+    def label_name_latin = self.name_romaji.present? ? self.name_romaji : self.name_kakasi
   end
 end # JapaneseLabels
