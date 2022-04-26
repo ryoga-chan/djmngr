1ee465dc2bdb5bd65b0c12f029bb4e99fc532dca
Ryoga Hibiki <142535628+ryoga-chan@users.noreply.github.com>
2022-04-26 22:15:28 +0200

    use ApplicationController#default_url_options for :ereader format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   layout -> { return 'ereader' if request.format.to_sym == :ereader }
   
+  def default_url_options
+    return { format: :ereader } if request.format.to_sym == :ereader
+    {}
+  end # default_url_options
+  
   def html_redirect_to(url_or_params, delay: 0)
     render html: <<~HTML.html_safe
       <!DOCTYPE html><html><head>
