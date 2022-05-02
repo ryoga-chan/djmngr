001e525c7af3b9234bef13d0ec599a7afc0084bb
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-05-02 15:13:27 +0200

    doujinshi - fave author/circle/doujin

--- db/migrate/20220128174429_create_authors.rb
+++ db/migrate/20220128174429_create_authors.rb
@@ -1,15 +1,17 @@
 class CreateAuthors < ActiveRecord::Migration[7.0]
   def change
     create_table :authors do |t|
-      t.string  :name
-      t.string  :name_romaji
-      t.string  :name_kana
-      t.string  :name_kakasi
-      t.text    :info
-      t.text    :aliases
-      t.text    :links
-      t.integer :doujinshi_org_id
-      t.string  :doujinshi_org_url, null: false, default: ''
+      t.string   :name
+      t.string   :name_romaji
+      t.string   :name_kana
+      t.string   :name_kakasi
+      t.text     :info
+      t.text     :aliases
+      t.text     :links
+      t.integer  :doujinshi_org_id
+      t.string   :doujinshi_org_url, null: false, default: ''
+      t.boolean  :favorite, default: false
+      t.datetime :faved_at
 
       t.timestamps
     end
