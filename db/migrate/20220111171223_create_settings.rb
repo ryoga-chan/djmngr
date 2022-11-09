02e36a4269a9a9d84d14961f3dbffb443ed199ef
Ryoga Hibiki <142535628+ryoga-chan@users.noreply.github.com>
2022-11-09 21:57:12 +0100

    settings - comics_viewer, file_manager

--- db/migrate/20220111171223_create_settings.rb
+++ db/migrate/20220111171223_create_settings.rb
@@ -8,13 +8,15 @@ class CreateSettings < ActiveRecord::Migration[7.0]
       t.timestamps
     end
     
-    Setting.create key: 'dir.to_sort', value: Rails.root.join('dj-libray', 'to-sort').to_s, notes: 'folder containing doujinshi to process'
-    Setting.create key: 'dir.sorting', value: Rails.root.join('dj-libray', 'sorting').to_s, notes: 'folder containing doujinshi under process'
-    Setting.create key: 'dir.sorted' , value: Rails.root.join('dj-libray', 'sorted' ).to_s, notes: 'folder containing processed doujinshi'
-    Setting.create key: 'reading_direction', value: 'r2l', notes: 'default reading direction' # r2l, l2r
-    Setting.create key: 'epub_img_width' , value: 768 , notes: 'resize image to this width in epub creation'
-    Setting.create key: 'epub_img_height', value: 1024, notes: 'resize image to this height in epub creation'
-    Setting.create key: 'scraper_useragent', notes: 'user agent used for doujinshi.org scraping',
+    Setting.create key: 'dir.to_sort'      , value: Rails.root.join('dj-libray', 'to-sort').to_s, notes: 'folder containing doujinshi to process'
+    Setting.create key: 'dir.sorting'      , value: Rails.root.join('dj-libray', 'sorting').to_s, notes: 'folder containing doujinshi under process'
+    Setting.create key: 'dir.sorted'       , value: Rails.root.join('dj-libray', 'sorted' ).to_s, notes: 'folder containing processed doujinshi'
+    Setting.create key: 'reading_direction', value: 'r2l'      , notes: 'default reading direction' # r2l, l2r
+    Setting.create key: 'epub_img_width'   , value: 768        , notes: 'resize image to this width in epub creation'
+    Setting.create key: 'epub_img_height'  , value: 1024       , notes: 'resize image to this height in epub creation'
+    Setting.create key: 'comics_viewer'    , value: 'mcomix -f', notes: 'application for reading CBZ files (comic book viewer)'
+    Setting.create key: 'file_manager'     , value: 'thunar'   , notes: 'application for browsing files (file manager)'
+    Setting.create key: 'scraper_useragent',                     notes: 'user agent used for doujinshi.org scraping',
       value: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
   end
 end
