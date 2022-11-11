b3251f43f6f9ac50a20feb6c6e2ba9a471883c5a
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-11-11 18:39:28 +0100

    * doujinshi/search - search in DeletedDoujinshi too * fix Doujin.search kakasi term usage

--- app/models/deleted_doujin.rb
+++ app/models/deleted_doujin.rb
@@ -1,2 +1,35 @@
 class DeletedDoujin < ApplicationRecord
+  def self.search(terms)
+    fname_info    = terms.to_s.strip    .parse_doujin_filename
+    tokens_orig   = terms.to_s          .tokenize_doujin_filename.join '%'
+    tokens_kakasi = terms.to_s.to_romaji.tokenize_doujin_filename.join '%'
+    
+    return self.none if tokens_orig.size < 3
+    
+    # search all terms on the original filename
+    rel_conditions = [
+      DeletedDoujin.where("name            LIKE ?", "%#{tokens_orig  }%"),
+      DeletedDoujin.where("name_kakasi     LIKE ?", "%#{tokens_kakasi}%"),
+      DeletedDoujin.where("alt_name        LIKE ?", "%#{tokens_orig  }%"),
+      DeletedDoujin.where("alt_name_kakasi LIKE ?", "%#{tokens_kakasi}%"),
+    ]
+    
+    # search only filename terms
+    if fname_info[:fname].present?
+      tokens_orig   = fname_info[:fname].to_s          .tokenize_doujin_filename.join '%'
+      tokens_kakasi = fname_info[:fname].to_s.to_romaji.tokenize_doujin_filename.join '%'
+      rel_conditions += [
+        DeletedDoujin.where("name            LIKE ?", "%#{tokens_orig  }%"),
+        DeletedDoujin.where("name_kakasi     LIKE ?", "%#{tokens_kakasi}%"),
+        DeletedDoujin.where("alt_name        LIKE ?", "%#{tokens_orig  }%"),
+        DeletedDoujin.where("alt_name_kakasi LIKE ?", "%#{tokens_kakasi}%"),
+      ]
+    end
+    
+    # build query with all conditions in OR
+    rel = rel_conditions.shift
+    rel_conditions.each{|cond| rel = rel.or cond }
+    
+    rel.order(Arel.sql "COALESCE(NULLIF(alt_name_kakasi, ''), NULLIF(name_kakasi, ''))")
+  end # self.search
 end
