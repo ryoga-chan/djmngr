75438306945f3c2cb42b6f0bb4f16cca7145f235
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-12-01 13:03:27 +0100

    use rails_real_favicon gem from realfavicongenerator.net

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
+// https://github.com/rails/sprockets/blob/main/UPGRADING.md#manifestjs
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
+//= link favicon/browserconfig.xml
