8d7f6e5eb6a826c502e9ae8fbb0fefd2bd1ba85b
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-12-05 21:07:09 +0100

    disable logging to file, use STDOUT only

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # log only to SDOUT (copied from config/environments/production.rb)
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
 end
