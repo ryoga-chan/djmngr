fe28dd63cbcc30fbc3e9daa70c072c3dc956f766
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-12-08 20:48:30 +0100

    * NSFW mode (hides all images)
    # Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
    * mobile top toolbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,21 +21,36 @@
 new Freezeframe(); // show image animation only on mouseover
 
 $.myapp = {
+  nsfw_mode_setup: function () {
+    var is_nsfw = localStorage['djmngr.nsfw-mode'] == 'true';
+    $('body').toggleClass('nsfw-mode', is_nsfw);
+    $('.nsfw-mode-toggle').find('i').text(is_nsfw ? 'remove_moderator' : 'shield');
+  },//nsfw_mode_setup
+  
   show_loading: function () {
     $('section.main-content').
       html('<progress class="progress is-primary" max="100">loading...</progress>');
   }// show_loading
 }//$.myapp
 
-$('#appNavbar a[href]').click(function (ev) {
+$('#appNavbar a[href]:not(.no-spinner)').click(function (ev) {
   if (!ev.ctrlKey && !ev.altKey)
     $.myapp.show_loading();
 });
 
-// temporary disable "run external program" button
+// temporarily disable "run external program" button when clicked
 $('body').on('click', '.run-progr', function (ev) {
   var bt = $(this).addClass('is-loading');
   setTimeout(function () { bt.removeClass('is-loading') }, 5000);
+});
+
+// apply sfw-mode if previously selected
+$.myapp.nsfw_mode_setup();
+// bind toggle sfw-mode button
+$('a.nsfw-mode-toggle').click(function (ev) {
+  ev.preventDefault();
+  localStorage['djmngr.nsfw-mode'] = !(localStorage['djmngr.nsfw-mode'] == 'true');
+  $.myapp.nsfw_mode_setup();
 });
 // -----------------------------------------------------------------------------
 }); })(jQuery)
