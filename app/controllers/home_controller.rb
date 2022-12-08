eacd81c58f6914c56674210f2603b1926e17427c
Ryoga Hibiki <142535628+ryoga-chan@users.noreply.github.com>
2022-12-08 17:26:00 +0100

    * home: show dj and processing total size
    # Ryoga Hibiki <142535628+ryoga-chan@users.noreply.github.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,16 +6,19 @@ class HomeController < ApplicationController
 
   def index
     @stats = {
-      dj:     Doujin.count,
-      dj_fav: Doujin.where(favorite: true).count,
-      dj_cat: Doujin.group(:category).select("category, COUNT(1) AS n").
-                      inject({}){|h, d| h.merge d.category => d.n },
-      todo:   ProcessIndexRefreshJob.entries.size,
-      wip:    Dir[ File.join Setting['dir.sorting'], '*', 'info.yml' ].size,
-      epub:   Dir[ Rails.root.join 'public', 'epub', '*.epub' ].size,
-      mda:    Author.count,
-      mdc:    Circle.count,
-      mdt:    Theme.count,
+      dj:         Doujin.count,
+      dj_size:    Doujin.sum(:size),
+      dj_fav:     Doujin.where(favorite: true).count,
+      dj_cat:     Doujin.group(:category).
+                    select("category, COUNT(1) AS n").
+                    inject({}){|h, d| h.merge d.category => d.n },
+      todo:       ProcessIndexRefreshJob.entries.size,
+      todo_size:  ProcessableDoujin.sum(:size),
+      wip:        Dir[ File.join Setting['dir.sorting'], '*', 'info.yml' ].size,
+      epub:       Dir[ Rails.root.join 'public', 'epub', '*.epub' ].size,
+      mda:        Author.count,
+      mdc:        Circle.count,
+      mdt:        Theme.count,
     }
     @stats[:md_tot] = @stats[:mda]+@stats[:mdc]+@stats[:mdt]
     
