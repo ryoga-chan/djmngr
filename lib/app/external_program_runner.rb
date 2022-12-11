d943fc55f3735ca207b8e5cd4b299de1c6505e7c
Ryoga Hibiki <142535628+ryoga-chan@users.noreply.github.com>
2022-12-11 14:54:49 +0100

    doujin/show - bt open in terminal

--- lib/app/external_program_runner.rb
+++ lib/app/external_program_runner.rb
@@ -1,12 +1,12 @@
 module ExternalProgramRunner
-  def self.run(program, file)
+  def self.run(program, target)
     env = Setting.get_json(:ext_cmd_env).to_h
     
     case program
-      when 'comics_viewer'; system env, %Q| #{Setting[:comics_viewer]} #{file.shellescape} & |
-      when 'file_manager' ; system env, %Q| #{Setting[:file_manager ]} #{file.shellescape} & |
-      when 'image_editor' ; system env, %Q| #{Setting[:image_editor ]} #{file.shellescape} & |
-      when 'terminal'     ; system env, %Q| #{Setting[:terminal     ]} & |, chdir: file
+      when 'comics_viewer'; system env, %Q| #{Setting[:comics_viewer]} #{target.shellescape} & |
+      when 'file_manager' ; system env, %Q| #{Setting[:file_manager ]} #{target.shellescape} & |
+      when 'image_editor' ; system env, %Q| #{Setting[:image_editor ]} #{target.shellescape} & |
+      when 'terminal'     ; system env, %Q| #{Setting[:terminal     ]} & |, chdir: target
     end
   end # run
 end # ExternalProgramRunner
