ee611114a2cb113f29d7f078cdfdd2596cd73256
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-12-11 19:07:07 +0100

    Vips::Image#scale_and_crop_to_offset_perc - use widht.ceil

--- lib/core_ext/vips/operations.rb
+++ lib/core_ext/vips/operations.rb
@@ -9,10 +9,11 @@ module CoreExt
         # the first parameter is the width of a square of size `width`x`width`
         # and we want a max height of `h`, so:
         #   x / 360 = image_w / image_h  ==>  x = image_w*360/image_h
-        img_scaled = self.thumbnail_image self.width*h/self.height, height: h
+        square_side = (self.width.to_f*h/self.height).ceil
+        img_scaled = self.thumbnail_image square_side, height: h
 
         # https://stackoverflow.com/questions/10853119/chop-image-into-tiles-using-vips-command-line/11098420#11098420
-        x_offset   = (img_scaled.width - w) * p / 100
+        x_offset   = ((img_scaled.width - w) * p.to_f / 100).to_i
         img_scaled.extract_area x_offset, 0, w, h
       end
     end
