075feccaf4abe1dcd539b92243f277848b842ab0
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2022-12-20 13:14:09 +0100

    ActiveRecord::Base.truncate_and_restart_sequence

---
+++ lib/core_ext/active_record/db.rb
@@ -0,0 +1,19 @@
+# https://stackoverflow.com/questions/13173618/truncate-tables-with-rails-console/13173994#13173994
+module CoreExt
+  module ActiveRecord
+    module Db
+      def self.included(base)
+        base.extend ClassMethods
+      end
+      
+      module ClassMethods
+        def truncate_and_restart_sequence
+          delete_all
+          connection.execute %Q|UPDATE SQLITE_SEQUENCE SET seq = 0 WHERE name = '#{table_name}'|
+        end # truncate_and_restart_sequence
+      end # ClassMethods
+    end
+  end
+end
+
+ActiveRecord::Base.send :include, CoreExt::ActiveRecord::Db
