4b80f1bc830ee1f7c69678652524d1f86147a17c
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2023-02-12 22:25:13 +0100

    * add navbar spinner onclick to any internal link and form submit * process/edit - add shortcut to update info in "move" tab

--- app/assets/javascripts/process.js
+++ app/assets/javascripts/process.js
@@ -70,11 +70,12 @@ if ($('body').data('ctrl') +'/'+ $('body').data('action') == 'process/edit') {
   });
   // add page shortcuts - tab move
   $.myapp.shortcuts.push({ key: 't', ctrl: false, alt: false, descr: 'focus on raw title', action: function (ev) { $('#dest_title').focus(); } });
-  $.myapp.shortcuts.push({ key: 'r', ctrl: false, alt: true, descr: 'copy kanji title to filename'  , action: function (ev) { $('#dest_filename').val( $('#dest_title'       ).val().trim() + '.zip' ); } });
-  $.myapp.shortcuts.push({ key: 't', ctrl: false, alt: true, descr: 'copy romaji title to filename' , action: function (ev) { $('#dest_filename').val( $('#dest_title_romaji').val().trim() + '.zip' ); } });
-  $.myapp.shortcuts.push({ key: 'e', ctrl: false, alt: true, descr: 'copy english title to filename', action: function (ev) { $('#dest_filename').val( $('#dest_title_eng'   ).val().trim() + '.zip' ); } });
+  $.myapp.shortcuts.push({ key: 'r', ctrl: false, alt: true , descr: 'copy kanji title to filename'  , action: function (ev) { $('#dest_filename').val( $('#dest_title'       ).val().trim() + '.zip' ); } });
+  $.myapp.shortcuts.push({ key: 't', ctrl: false, alt: true , descr: 'copy romaji title to filename' , action: function (ev) { $('#dest_filename').val( $('#dest_title_romaji').val().trim() + '.zip' ); } });
+  $.myapp.shortcuts.push({ key: 'e', ctrl: false, alt: true , descr: 'copy english title to filename', action: function (ev) { $('#dest_filename').val( $('#dest_title_eng'   ).val().trim() + '.zip' ); } });
   $.myapp.shortcuts.push({ key: 'l', ctrl: false, alt: false, descr: 'convert all titles to lowercase', action: $.myapp.titles_to_lowercase });
-  $.myapp.shortcuts.push({ key: 'Enter', ctrl: false, alt: true, descr: 'finalize archive', action: function (ev) { $('.actions button[value="finalize"]').get(0).click(); } });
+  $.myapp.shortcuts.push({ key: 'Enter', ctrl: false, alt: true , descr: 'finalize archive', action: function (ev) { $('.actions button[value="finalize"]').get(0).click(); } });
+  $.myapp.shortcuts.push({ key: 'Enter', ctrl: true , alt: false, descr: 'update info', action: function (ev) { $('.actions button[value="save"]').get(0).click(); } });
   // add page shortcuts - scoring
   $.myapp.shortcuts.push({ key: '-', ctrl: false, alt: false, descr: 'clear scoring', action: function (ev) { $('span.clear-score').get(0).click(); } });
   for (let i = 1; i <= 9; i++)
