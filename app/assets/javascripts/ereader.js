3b054fddb5ef98a239bbb74e6b82828d4f705aa7
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2023-07-04 21:11:23 +0200

    sequential image load for all formats (html, ereader)

--- app/assets/javascripts/ereader.js
+++ app/assets/javascripts/ereader.js
@@ -1,20 +1,3 @@
 //= require ./libs/jquery
 
-(function($){
-  // sequentially load the next image
-  window.load_next_image = function (ev_type, caller) {
-    var img = $('img.async_load').filter(':not(.loaded)').first();
-    
-    if (img.length == 1)
-      img.addClass('loaded').attr('src', img.data('url'));
-  }//load_next_image
-
-  $(function () {
-    // sequential image loading
-    $('img.async_load').
-      attr('decoding', 'async').
-      attr('onload' , "load_next_image('ok' , this)").
-      attr('onerror', "load_next_image('err', this)");
-    load_next_image('init');
-  });
-})(jQuery)
+//= require ./components/img_sequential_load
