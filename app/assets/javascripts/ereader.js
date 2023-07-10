21dfdcb7804618883dedc13b8acbf34121a5c848
Ryoga Hibiki <142535628+ryoga-chan@users.noreply.github.com>
2023-07-10 21:43:00 +0200

    on screen keyabord for ereader devices

--- app/assets/javascripts/ereader.js
+++ app/assets/javascripts/ereader.js
@@ -1,3 +1,10 @@
 //= require ./libs/jquery
 
 //= require ./components/img_sequential_load
+//= require ./components/jQKeyboard
+
+(function ($) { $(function () {
+// -----------------------------------------------------------------------------
+$('.jQKeyboardToggle').initKeypad();
+// -----------------------------------------------------------------------------
+}); })(jQuery);
