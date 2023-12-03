211898ce318f06910be0a373d3afb0830cf9f064
Ryoga Hibiki <142535628+ryoga-chan@users.noreply.github.com>
2023-12-03 23:58:14 +0100

    process/index preview - fix preview generation for two pages

--- app/jobs/process_index_preview_job.rb
+++ app/jobs/process_index_preview_job.rb
@@ -2,7 +2,7 @@ class ProcessIndexPreviewJob < ProcessIndexRefreshJob
   THUMB_WIDTH  = 320
   THUMB_HEIGHT = 640
   THUMBS_CHUNK = 3
-  THUMBS_NUM   = ProcessController::ROWS_PER_PAGE * 2
+  THUMBS_NUM   = ProcessController::ROWS_PER_PAGE
   
   def self.rm_previews
     pattern = Rails.root.join('public', ProcessableDoujin::THUMB_FOLDER, '*.webp').to_s
@@ -33,51 +33,59 @@ class ProcessIndexPreviewJob < ProcessIndexRefreshJob
     
     rel = self.class.entries(order: order)
     rel = rel.where(id: id) if id
-    rel.page(page.to_i).per(THUMBS_NUM).each do |processable_doujin|
-      next if File.exist?(processable_doujin.thumb_path)
-      
-      fname = File.join Setting['dir.to_sort'], processable_doujin.name
-      next unless File.exist?(fname)
-      
-      Zip::File.open(fname) do |zip|
-        zip_images = zip.image_entries(sort: true)
-        processable_doujin.update images: zip_images.size
-        
-        thumb_entries = zip_images.pages_preview(chunk_size: THUMBS_CHUNK)
-        
-        images = thumb_entries.map{|e|
-          i = Vips::Image.webp_cropped_thumb(
-            e.get_input_stream.read,
-            width:   THUMB_WIDTH,
-            height:  THUMB_HEIGHT,
-            padding: false
-          )[:image]
-          
-          # https://github.com/libvips/pyvips/issues/202
-          # https://github.com/libvips/libvips/issues/1525
-          i = i.colourspace('srgb') if i.bands  < 3
-          i = i.bandjoin(255)       if i.bands == 3
-          
-          i
-        }.compact
-        
-        # display missing image if no images are found
-        images << self.class.img_not_found if images.empty?
-        
-        # use transparent images for the remaining thumbs
-        num_fill = (images.size.to_f / 3).ceil * 3
-        images += (images.size ... num_fill).map{ self.class.img_transparent }
-        
-        # https://github.com/libvips/ruby-vips/blob/master/lib/vips/methods.rb#L362
-        # create a long thumbnail for desktop
-        collage = Vips::Image.arrayjoin images, background: 0
-        collage.write_to_file processable_doujin.thumb_path(mobile: false)
-        # create a portrait thumbnail for mobile
-        collage = Vips::Image.arrayjoin images, background: 0, across: THUMBS_CHUNK
-        collage.write_to_file processable_doujin.thumb_path(mobile: true)
+    
+    # generate preview for two pages
+    [page.to_i, page.to_i+1].each do |p|
+      rel.page(p).per(THUMBS_NUM).each do |processable_doujin|
+        self.class.generate_preview processable_doujin
       end
     end
 
     self.class.rm_lock_file
   end # perform
+  
+  def self.generate_preview(processable_doujin)
+    return if File.exist?(processable_doujin.thumb_path)
+    
+    fname = File.join Setting['dir.to_sort'], processable_doujin.name
+    return unless File.exist?(fname)
+    
+    Zip::File.open(fname) do |zip|
+      zip_images = zip.image_entries(sort: true)
+      processable_doujin.update images: zip_images.size
+      
+      thumb_entries = zip_images.pages_preview(chunk_size: THUMBS_CHUNK)
+      
+      images = thumb_entries.map{|e|
+        i = Vips::Image.webp_cropped_thumb(
+          e.get_input_stream.read,
+          width:   THUMB_WIDTH,
+          height:  THUMB_HEIGHT,
+          padding: false
+        )[:image]
+        
+        # https://github.com/libvips/pyvips/issues/202
+        # https://github.com/libvips/libvips/issues/1525
+        i = i.colourspace('srgb') if i.bands  < 3
+        i = i.bandjoin(255)       if i.bands == 3
+        
+        i
+      }.compact
+      
+      # display missing image if no images are found
+      images << self.class.img_not_found if images.empty?
+      
+      # use transparent images for the remaining thumbs
+      num_fill = (images.size.to_f / 3).ceil * 3
+      images += (images.size ... num_fill).map{ self.img_transparent }
+      
+      # https://github.com/libvips/ruby-vips/blob/master/lib/vips/methods.rb#L362
+      # create a long thumbnail for desktop
+      collage = Vips::Image.arrayjoin images, background: 0
+      collage.write_to_file processable_doujin.thumb_path(mobile: false)
+      # create a portrait thumbnail for mobile
+      collage = Vips::Image.arrayjoin images, background: 0, across: THUMBS_CHUNK
+      collage.write_to_file processable_doujin.thumb_path(mobile: true)
+    end
+  end # self.generate_preview
 end
