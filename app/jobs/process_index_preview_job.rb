cc0dd6478f39bbe60a43500417c331f8b5f78b0a
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2024-02-02 19:08:14 +0100

    ProcessIndexPreviewJob - double page preview for group order too

--- app/jobs/process_index_preview_job.rb
+++ app/jobs/process_index_preview_job.rb
@@ -6,7 +6,7 @@ class ProcessIndexPreviewJob < ProcessIndexRefreshJob
     Dir[pattern].each{|f| FileUtils.rm_f f }
   end # self.rm_previews
   
-  def perform(id: nil, order: nil, page: nil)
+  def perform(order: nil, page: nil, id: nil)
     page = 1 if page.to_i <= 0
     
     rel = self.class.entries(order: order)
@@ -14,13 +14,15 @@ class ProcessIndexPreviewJob < ProcessIndexRefreshJob
     
     records = []
     if order.to_s.starts_with?('group')
-      records  = rel.page(p.to_i).per(ProcessController::GROUP_EPP).to_a
+      # generate preview for two pages
+      records  = rel.page(page.to_i  ).per(ProcessController::GROUP_EPP).to_a
+      records += rel.page(page.to_i+1).per(ProcessController::GROUP_EPP).to_a
       # generate preview for child records
       records += records.inject([]){|a,r| a.concat r.processable_doujin_childs }
     else
       # generate preview for two pages
-      records  = rel.page(p.to_i).per(Setting[:process_epp].to_i).to_a
-      records += rel.page(p.to_i+1).per(Setting[:process_epp].to_i).to_a
+      records  = rel.page(page.to_i  ).per(Setting[:process_epp].to_i).to_a
+      records += rel.page(page.to_i+1).per(Setting[:process_epp].to_i).to_a
     end
     
     records.each_with_index do |processable_doujin, i|
