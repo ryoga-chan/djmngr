82f23535f854df085a8614b1fd8664da514fca60
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2024-02-11 16:54:03 +0100

    Ws::EHentai - add ex-hentai support (auth+save cookies), try detecting english title

--- app/assets/javascripts/controllers/process.js
+++ app/assets/javascripts/controllers/process.js
@@ -202,10 +202,15 @@ if ($('body').data('ctrl') +'/'+ $('body').data('action') == 'process/edit') {
     $(this).parents('form:first').submit();
   });
 
-  // ehentai search callback: place titles in kanji/romaji fields, and full text in notes field
+  // ehentai search callback: place titles in kanji/romaji/eng fields, and full text in notes field
   $('.ehentai-search').data('onselect', function (info) {
     $('#dest_title').val(info.title_jpn_clean || info.title_clean);
     $('#notes').append( $('<div/>').html(info.title_jpn || info.title).text() + "\n" );
+    
+    if (info.title_eng) {
+      $('#dest_title_eng').val(info.title_eng_clean);
+      $('#notes').append( 'ENG: ' + $('<div/>').html(info.title_eng).text() + "\n" );
+    }//if
     
     if (info.title_jpn) {
       $('#dest_title_romaji').val(info.title_clean);
