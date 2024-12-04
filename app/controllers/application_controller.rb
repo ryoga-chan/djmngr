56911de6f114f83627360b83a0342b96c3638502
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2024-12-04 20:30:21 +0100

    allow_browser - set before_action except for :ereader format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  allow_browser versions: :modern, if: -> { request.format.symbol != :ereader }
 
   def default_url_options
     opts = {}
