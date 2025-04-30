332641abdb43ceca1bd79fd70cf6df3853cd840d
Ryoga Hibiki <ryoga-chan@users.noreply.github.com>
2025-04-30 11:50:32 +0200

    process/index - highlight multiple spaces in filename

--- app/helpers/process_helper.rb
+++ app/helpers/process_helper.rb
@@ -4,4 +4,11 @@ module ProcessHelper
       inline_image_tag(:'image/webp', Base64.encode64(File.binread fname), options) :
       :MISS
   end # thumbnail_tag
+
+  # highlight multiple spaces where present
+  def hl_multispace(str)
+    h(str).gsub(/( {2,})/) do |m|
+      %Q|<span class="has-background-warning">#{'&nbsp;' * m.size}</span>|
+    end.html_safe
+  end # hl_multispace
 end
